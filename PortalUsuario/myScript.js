1cb1046683e6f55a241a34162c8f225b8f0e48ae
David E. Ochoa Diaz <davideochoa@gmail.com>
2021-08-31 19:10:19 -0500

    Primer commite

---
+++ PortalUsuario/myScript.js
@@ -0,0 +1,8 @@
+function oyenteCargarPagina() {
+    document.getElementById('modalLogin').style.display='block'
+}
+
+function oyenteBotonLogin() {
+    document.getElementById('modalLogin').style.display='none'
+}
+
