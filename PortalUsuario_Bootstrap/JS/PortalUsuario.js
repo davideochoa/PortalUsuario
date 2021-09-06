568fccf57587474fcd889bcd6ecdd5069d9a8ac4
David E. Ochoa Diaz <davideochoa@gmail.com>
2021-09-06 00:20:16 -0500

    LOGIN OK

---
+++ PortalUsuario_Bootstrap/JS/PortalUsuario.js
@@ -0,0 +1,8 @@
+function oyenteCargarPagina() {
+    document.getElementById('modalLogin').style.display='block'
+}
+
+function oyenteBotonLogin() {
+    console.info("Evento oyenteBotonLogin()");
+    document.getElementById('modalLogin').style.display='none'
+}
